16b72cf5acc447db93cb294313fb265f21b29ee7
thatneat <thatneat@users.noreply.github.com>
2014-08-20 08:43:55 -0700

    Update example_ai.py

--- brains/example_ai.py
+++ brains/example_ai.py
@@ -5,7 +5,7 @@ def random_ai_brain_fn(player, game, spied_card):
     '''The most sophisticated Brave Rats AI ever written
 
     :param player: a Player instance
-    :param game: a Game instance, used to get
+    :param game: a Game instance, used to look up info about played cards, score, etc.
     :param spied_card: If I successfully played a spy last turn, this is the card that the opponent has revealed and
         will play. Otherwise, None
     :return: a card from my player's hand with which to vanquish my opponent.
