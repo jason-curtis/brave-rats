bc4322a3173d4f6dbe1462c65970f33daffffd26
thatneat <thatneat@gmail.com>
2014-09-16 18:23:48 -0700

    Easier signature for colorization

--- components/style.py
+++ components/style.py
@@ -1,5 +1,22 @@
 CLI_COLORS={
     'red': '\x1b[31m',
-    'blue' : '\033[94m',
-    'end' : '\033[0m'
+    'blue': '\033[94m',
+    'end': '\033[0m'
     }
+
+
+def redify(str_):
+    return _colorize(str_, 'red')
+
+
+def blueify(str_):
+    return _colorize(str_, 'blue')
+
+
+def _colorize(str_, color_name):
+    '''
+    :param str_: String to be colorized
+    :param color_name: a color from CLI_COLORS
+    :return: str_, wrapped with appropriate flags so that when printed it will be colorized
+    '''
+    return '{}{}{}'.format(CLI_COLORS[color_name], str_, CLI_COLORS['end'])
